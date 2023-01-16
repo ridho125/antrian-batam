a73dc1477bb2cfb78f81db3e43681a51cf53ed1c
C1 Aris Wardhana <180376@maxxis.id>
2023-01-16 14:38:22 +0700

    Update database

--- database/db_antrian.sql
+++ database/db_antrian.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jun 13, 2021 at 03:17 AM
--- Server version: 5.7.34
--- PHP Version: 8.0.6
+-- Host: 127.0.0.1
+-- Generation Time: Jan 16, 2023 at 08:37 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,12 +28,42 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tbl_antrian` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint NOT NULL,
   `tanggal` date NOT NULL,
-  `no_antrian` smallint(6) NOT NULL,
+  `no_antrian` smallint NOT NULL,
+  `no_lambung` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
   `status` enum('1','0') NOT NULL DEFAULT '0',
   `updated_date` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_antrian`
+--
+
+INSERT INTO `tbl_antrian` (`id`, `tanggal`, `no_antrian`, `no_lambung`, `status`, `updated_date`) VALUES
+(4, '2023-01-16', 1, 'FR005', '1', '2023-01-16 14:32:14'),
+(5, '2023-01-16', 2, 'VV403', '0', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `userid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `userid`, `password`) VALUES
+(4, 'Aris Wardhana', 'Wardhana666', '2d76da9ed599cbf3a0ca849f14c58115'),
+(5, 'Admin', 'admin01', '827ccb0eea8a706c4c34a16891f84e7b');
 
 --
 -- Indexes for dumped tables
@@ -46,6 +76,12 @@ ALTER TABLE `tbl_antrian`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -53,7 +89,13 @@ ALTER TABLE `tbl_antrian`
 -- AUTO_INCREMENT for table `tbl_antrian`
 --
 ALTER TABLE `tbl_antrian`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
